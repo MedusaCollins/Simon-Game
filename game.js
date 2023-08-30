9f6e3404fd792c8ffb067a9797da422b1f2420c5
Medusa Collins <enesfurkanceylan@hotmail.com>
2023-08-30 05:46:19 +0300

    Checking the User's Answer Against the Game Sequence

--- game.js
+++ game.js
@@ -1,41 +1,42 @@
 var gamePattern = [];
 var userClickedPattern = [];
+var buttonColours = ["red", "blue", "green", "yellow"];
 
-var buttonColours = ["red", "blue", "green", "yellow"]
-var randomNumber = Math.floor(Math.random() * 4);
-var randomChosenColour = buttonColours[randomNumber];
-var audio = new Audio('');
-var userChosenColour = '';
 var gameStarted = false;
 var level = 0;
 
 function nextSequence(){
+    userClickedPattern =  [];
     level++;
     $("h1").text("Level " + level)
-    randomNumber = Math.floor(Math.random() * 4);
-    randomChosenColour = buttonColours[randomNumber];
-    gamePattern.push(buttonColours[randomNumber])
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour)
     playSound(randomChosenColour)
 
     $("#"+ randomChosenColour).fadeOut().fadeIn();
-    
+    console.log("nextSequence")
 }
 $(".btn").click(function (e) {
     userChosenColour = e.target.id;
     userClickedPattern.push(userChosenColour);
-    console.log(userClickedPattern);
+
     playSound(userChosenColour);
     animatePress(userChosenColour);
+
+      //2. Call checkAnswer() after a user has clicked and chosen their answer, passing in the index of the last answer in the user's sequence.
+    checkAnswer(userClickedPattern.length-1);
+    console.log("click")
 })
 
 function playSound(sound){
-    audio = new Audio("sounds/" + sound + ".mp3");
+    var audio = new Audio("sounds/" + sound + ".mp3");
     audio.play();
 }
 
 function animatePress(currentColour){
 $("." + currentColour).addClass("pressed")
-
 setTimeout(function(){
     $("." + currentColour).removeClass("pressed")
 }, 100)
@@ -47,3 +48,17 @@ $(document).keypress(function (e) {
         gameStarted = true;
     }
 });
+
+function checkAnswer(currentLevel){
+if( gamePattern[currentLevel] === userClickedPattern[currentLevel]){
+
+    console.log("Başarılı");
+
+    if(userClickedPattern.length === gamePattern.length){
+        setTimeout(function () {
+            nextSequence();
+          }, 1000);
+          console.log("checkAnswer")
+    }
+}else{ console.log("Başarısız")}
+}
